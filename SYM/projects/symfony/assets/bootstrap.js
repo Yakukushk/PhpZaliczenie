f21df69bd0f72a27bbedf1291fb27f5a9c07b0f9
Yakukushk <daniilliubko@gmail.com>
2023-06-05 12:12:29 +0200

    Update Commit

---
+++ SYM/projects/symfony/assets/bootstrap.js
@@ -0,0 +1,7 @@
+// assets/bootstrap.js
+import 'bootstrap';
+import './styles/bootstrap.scss';
+
+// Optional: If you need to use jQuery globally
+import $ from 'jquery';
+global.$ = global.jQuery = $;
