2ba3355dc9e035190af13101f0a2fb59997c9d3c
Yakukushk <daniilliubko@gmail.com>
2023-06-07 20:22:10 +0200

    Update Commit

--- SYM/projects/symfony/src/Controller/ProductController.php
+++ SYM/projects/symfony/src/Controller/ProductController.php
@@ -5,28 +5,32 @@ namespace App\Controller;
 
 // ...
 use App\Entity\Product;
+use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'create_product')]
-    public function createProduct(EntityManagerInterface $entityManager): Response
+    #[Route('/product/new', name: 'create_product')]
+    public function createProduct(Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(2000);
-        $product->setDescription('Mechanic and silence key!');
-        $products = $entityManager->getRepository(Product::class)->findAll();
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
 
-        return $this->render('product/index.html.twig', ['products' => $products]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('product/new.html.twig', ['form' => $form->createView()]);
+
     }
 
     #[Route('/products', name: 'product_list')]
--- SYM/projects/symfony/src/Form/ProductType.php
+++ SYM/projects/symfony/src/Form/ProductType.php
@@ -4,17 +4,26 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label' => 'Name'
+            ])
+            ->add('price', IntegerType::class, [
+                'label' => 'Price'
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description'
+            ])
+            ->add('save', SubmitType::class, ['label' => 'Add Product'])
         ;
     }
 
